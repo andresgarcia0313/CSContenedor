0e6c59137dde91888c029d6fafb9256de53e99e7
Andrés Eduardo García Márquez <andresgarcia0313@gmail.com>
2024-07-11 15:11:24 -0500

    Versión contenida para desarrollar con contenedor o sin contenedor y en cualquier S.O.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido");
+
+            Console.WriteLine("¿Cómo te llamas?");
+            string nombre = Console.ReadLine() ?? "Invitado";
+
+            var app = new HelloWorldApp();
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                app.Saludar(nombre);
+            }
+        }
+    }
+
+    class HelloWorldApp
+    {
+        public void Saludar(string nombre)
+        {
+            Console.WriteLine($"Hola, {nombre}! ¡Bienvenido a mi aplicación!");
+        }
+    }
+}
